7ca97ff759df88adc0a2fc2815214f416c485076
roman <romanhurt@seznam.cz>
2021-08-19 16:10:18 +0200

    odstranění loga

--- src/App.js
+++ src/App.js
@@ -8,16 +8,9 @@ function App() {
         
         <p>
           Edit <code>src/App.js</code> and save to reload. HELLO!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </p>
+   
+        </header>
     </div>
   );
 }
